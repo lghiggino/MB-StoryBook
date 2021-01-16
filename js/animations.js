5b57e211179fb402e162864b2a9cd2f3fb6f3ef6
Leonardo N Ghiggino <lghiggino@gmail.com>
2021-01-15 21:02:05 -0300

    basic card setup

---
+++ js/animations.js
@@ -0,0 +1,21 @@
+console.log("Boxes animation are on boxes.js")
+
+const boxes = document.querySelectorAll(".box")
+
+window.addEventListener("scroll", checkBoxes)
+
+checkBoxes()
+
+function checkBoxes(){
+    const triggerBottom = window.innerHeight / 7 * 4;
+    boxes.forEach(box => {
+        const boxTop = box.getBoundingClientRect().top;
+        
+        if (boxTop < triggerBottom){
+            box.classList.add("show");
+        }else {
+            box.classList.remove("show");
+        }
+    })
+}
+
