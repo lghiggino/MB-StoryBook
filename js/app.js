9f8b6ab720356daeba19772c8b839f5420eeeacb
Leonardo N Ghiggino <lghiggino@gmail.com>
2021-01-17 20:23:12 -0300

    RED ARROW Media Queries

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 // navbar footer items hover effect
-const footerItem = document.querySelectorAll(".hero-foot li");
+// const footerItem = document.querySelectorAll(".hero-foot li");
 
-footerItem.forEach(item => {
-    item.addEventListener("mouseenter", (e) => {
-        e.target.classList.add("is-active");
-    })
-    item.addEventListener("mouseleave", (e) => {
-        e.target.classList.remove("is-active");
-    })
-});
+// footerItem.forEach(item => {
+//     item.addEventListener("mouseenter", (e) => {
+//         e.target.classList.add("is-active");
+//     })
+//     item.addEventListener("mouseleave", (e) => {
+//         e.target.classList.remove("is-active");
+//     })
+// });
 
 //navbar burger menu
 // const navbarBurger = document.querySelector(".navbar-burger");
@@ -25,47 +25,59 @@ footerItem.forEach(item => {
 // })
 
 //Tabs navigation
-const tabs = document.querySelectorAll(".tabs li")
-const tabContentBoxes = document.querySelectorAll("#tab-content > div")
+// const tabs = document.querySelectorAll(".tabs li")
+// const tabContentBoxes = document.querySelectorAll("#tab-content > div")
 
-tabs.forEach(tab => {
-    tab.addEventListener("click", () => {
-        tabs.forEach(item => item.classList.remove("is-active"));
-        tab.classList.add("is-active");
+// tabs.forEach(tab => {
+//     tab.addEventListener("click", () => {
+//         tabs.forEach(item => item.classList.remove("is-active"));
+//         tab.classList.add("is-active");
 
-        const target = tab.dataset.target;
-        tabContentBoxes.forEach(box => {
-            if (box.getAttribute("id") === target){
-                box.classList.remove("is-hidden");
-            }else {
-                box.classList.add("is-hidden");
-            }
-        })
-    });
-});
+//         const target = tab.dataset.target;
+//         tabContentBoxes.forEach(box => {
+//             if (box.getAttribute("id") === target){
+//                 box.classList.remove("is-hidden");
+//             }else {
+//                 box.classList.add("is-hidden");
+//             }
+//         })
+//     });
+// });
 
 //Conquistas Dropdown
-const dropdownConquistas = document.querySelector("#dropdown-conquistas")
-const dropdownConquistasItens = document.querySelectorAll(".dropdown-item") 
-const dropdownConquistasDivs = document.querySelectorAll(".tall")
-const dropdownTile = document.querySelector("#dropdown-tile")
+// const dropdownConquistas = document.querySelector("#dropdown-conquistas")
+// const dropdownConquistasItens = document.querySelectorAll(".dropdown-item") 
+// const dropdownConquistasDivs = document.querySelectorAll(".tall")
+// const dropdownTile = document.querySelector("#dropdown-tile")
 
-dropdownConquistas.addEventListener("click", () => {
-    dropdownConquistas.classList.toggle("is-active");
-})
+// dropdownConquistas.addEventListener("click", () => {
+//     dropdownConquistas.classList.toggle("is-active");
+// })
 
-dropdownConquistasItens.forEach(item => {
-    item.addEventListener("click", () => {
-        const year = item.dataset.year
-        //console.log(year)
-        dropdownConquistasDivs.forEach(div => {
-            if(div.getAttribute("id") === year){
-                div.classList.remove("is-hidden");
-            }else {
-                div.classList.add("is-hidden")
-            }
-        })
-    })
-})
+// dropdownConquistasItens.forEach(item => {
+//     item.addEventListener("click", () => {
+//         const year = item.dataset.year
+//         //console.log(year)
+//         dropdownConquistasDivs.forEach(div => {
+//             if(div.getAttribute("id") === year){
+//                 div.classList.remove("is-hidden");
+//             }else {
+//                 div.classList.add("is-hidden")
+//             }
+//         })
+//     })
+// })
     
-   
+const firstDiamondButton = document.querySelector("#do-you-have-a-website-submit");
+const firstCheckIdButton = document.querySelector("#firstCheckId");
+const secondCheckIdButton = document.querySelector("#secondCheckId" );
+const thirdCheckIdButton = document.querySelector("#thirdCheckId" );
+const fourthCheckIdButton = document.querySelector("#fourthCheckId");
+
+firstDiamondButton.addEventListener("click", () => {
+   console.log(
+    firstCheckIdButton.checked,
+    secondCheckIdButton.checked,
+    thirdCheckIdButton.checked, 
+    fourthCheckIdButton.checked) 
+})
