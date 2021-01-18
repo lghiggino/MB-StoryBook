9f87dc9dc10e54eb99cb794603dee464b6978cc6
Leonardo N Ghiggino <lghiggino@gmail.com>
2021-01-17 22:23:11 -0300

    spinning animation on red arrow

--- js/app.js
+++ js/app.js
@@ -1,50 +1,24 @@
-// navbar footer items hover effect
-// const footerItem = document.querySelectorAll(".hero-foot li");
+// Tabs navigation
+const tabs = document.querySelectorAll(".tabs li")
+const tabContentBoxes = document.querySelectorAll("#tab-content > div")
 
-// footerItem.forEach(item => {
-//     item.addEventListener("mouseenter", (e) => {
-//         e.target.classList.add("is-active");
-//     })
-//     item.addEventListener("mouseleave", (e) => {
-//         e.target.classList.remove("is-active");
-//     })
-// });
+tabs.forEach(tab => {
+    tab.addEventListener("click", () => {
+        tabs.forEach(item => item.classList.remove("is-active"));
+        tab.classList.add("is-active");
 
-//navbar burger menu
-// const navbarBurger = document.querySelector(".navbar-burger");
-// const navbarMenu = document.querySelector(".navbar-menu");
-// const navbarItens = document.querySelectorAll(".navbar-item");
+        const target = tab.dataset.target;
+        tabContentBoxes.forEach(box => {
+            if (box.getAttribute("id") === target){
+                box.classList.remove("is-hidden");
+            }else {
+                box.classList.add("is-hidden");
+            }
+        })
+    });
+});
 
-// navbarBurger.addEventListener("click", () => {
-//     navbarMenu.classList.toggle("is-active");
-//     navbarItens.forEach(item => {
-//         item.addEventListener("click", () => {
-//             navbarMenu.classList.remove("is-active");
-//         })
-//     })
-// })
-
-//Tabs navigation
-// const tabs = document.querySelectorAll(".tabs li")
-// const tabContentBoxes = document.querySelectorAll("#tab-content > div")
-
-// tabs.forEach(tab => {
-//     tab.addEventListener("click", () => {
-//         tabs.forEach(item => item.classList.remove("is-active"));
-//         tab.classList.add("is-active");
-
-//         const target = tab.dataset.target;
-//         tabContentBoxes.forEach(box => {
-//             if (box.getAttribute("id") === target){
-//                 box.classList.remove("is-hidden");
-//             }else {
-//                 box.classList.add("is-hidden");
-//             }
-//         })
-//     });
-// });
-
-//Conquistas Dropdown
+// Conquistas Dropdown
 // const dropdownConquistas = document.querySelector("#dropdown-conquistas")
 // const dropdownConquistasItens = document.querySelectorAll(".dropdown-item") 
 // const dropdownConquistasDivs = document.querySelectorAll(".tall")
