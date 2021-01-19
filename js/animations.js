0f8dd4b5921e54207170d3f521feb139478f5c57
Leonardo N Ghiggino <lghiggino@gmail.com>
2021-01-18 22:14:29 -0300

    paragraph animation

--- js/animations.js
+++ js/animations.js
@@ -34,17 +34,27 @@ function gainOpacity(){
     })
 }
 
-window.addEventListener("scroll", () => {
-    const body = document.querySelector("body")
-    console.log(window.scrollY)
-    console.log(window.scrollMaxY)
-    // console.log(body.scrollHeight)
-    // console.log(window.innerHeight)
-    if (window.scrollY / window.scrollMaxY >= 0.10){
-        body.style.backgroundColor = "red";
-    }
-})
+const paras = document.querySelectorAll(".para")
+window.addEventListener("scroll", paraShow)
+paraShow()
 
+function paraShow(){
+    const triggerBottom = window.innerHeight / 7 * 4;
+    paras.forEach(para => {
+        const boxTop = para.getBoundingClientRect().top;
+        
+        if (boxTop < triggerBottom){
+            para.classList.add("show");
+        }else {
+            para.classList.remove("show");
+        }
+    })
+}
+
+const allSections = document.querySelectorAll("section")
+    allSections.forEach(section => {
+        console.log(section.getBoundingClientRect())
+    })
 
 
 console.log("Red Arrows are on")
